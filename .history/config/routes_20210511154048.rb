3f21b56655db6d125135f8565986f167047f7076
Maurice Wong <cas0121202@coderacademy.edu.aulearclearlsgit initgit config --global usegit config --global user.email cas0121202@coderacademy.edu.au>
2021-05-11 15:59:08 +1000

    heroku setup

--- .history/config/routes_20210511154048.rb
+++ .history/config/routes_20210511154048.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'students#index'
-  
   resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
